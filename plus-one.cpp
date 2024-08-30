42a7fa1fdf10fab4f0648505bfce7907aab72315
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2024-08-30 10:23:00 +0530

    Delete plus-one.cpp

--- plus-one.cpp
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        k=int("".join(map(str,digits)))
-        k+=1
-        return map(int,(list(str(k))))
