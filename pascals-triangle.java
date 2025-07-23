2bbdf01a2c2ff8819286d4f5371b196b0e3632ae
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2025-07-23 12:20:09 +0530

    Add pascals-triangle.java

---
+++ pascals-triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < numRows; i++){
+            List<Integer> rows = new ArrayList<>();
+            if(i == 0){
+                rows.add(1);
+            }else if(i == 1){
+                rows.add(1);
+                rows.add(1);
+            }else{
+                rows.add(1);
+                // List<Integer> prev = res.get(i-1);
+                for(int j = 1; j < i; j++){
+                    rows.add(res.get(i-1).get(j-1) + res.get(i-1).get(j));
+                }rows.add(1);
+            }res.add(rows);
+        }
+        return res;
+    }
+}
