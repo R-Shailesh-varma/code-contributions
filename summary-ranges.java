ef047373f6e7d9ab722276c3620a872f8ac7b353
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2025-07-26 22:55:53 +0530

    Add summary-ranges.java

---
+++ summary-ranges.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+       List<String> res = new ArrayList<>();
+        if (nums.length == 0) return res;
+        int start = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+                if (start == i - 1) {
+                    res.add(String.valueOf(nums[start]));
+                } else {
+                    res.add(nums[start] + "->" + nums[i - 1]);
+                }
+                start = i;
+            }
+        }
+        return res;
+    }
+}
