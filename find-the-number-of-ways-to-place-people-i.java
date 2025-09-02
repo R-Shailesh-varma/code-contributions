5d39da98b886675d401b75a31c73c939ae25855e
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2025-09-02 12:28:37 +0530

    Add find-the-number-of-ways-to-place-people-i.java

---
+++ find-the-number-of-ways-to-place-people-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        Arrays.sort(points, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int ans = 0;
+        int n = points.length;
+        for(int i = 0; i < n; i++){
+            int maxY = Integer.MIN_VALUE;
+            for(int j = i+1; j < n ;j++){
+                if(points[j][1] <= points[i][1] && points[j][1] > maxY){
+                    ans++;
+                    maxY = points[j][1];
+                }
+            }
+        }return ans;
+    }
+}
