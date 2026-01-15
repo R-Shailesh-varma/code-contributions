4769272310cf633091da438eac576c5c398a890d
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2026-01-15 10:21:03 +0530

    Add decode-ways.java

---
+++ decode-ways.java
@@ -0,0 +1,20 @@
+class Solution {    
+    public int numDecodings(String s) {
+        if(s.charAt(0) == '0') return 0;
+        int one = 1;
+        int two = 1;
+        for(int i = 1; i < s.length(); i++){
+            int current = 0;
+            if(s.charAt(i) != '0'){
+                current = one;
+            }
+
+            int val = Integer.parseInt(s.substring(i-1, i+1));
+            if(val >= 10 && val <= 26) current += two;
+
+            two = one;
+            one = current;
+        }
+        return one;
+    }
+}
