b0aa72e2013aef7e25aee57e85f512c14617a4e8
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2026-01-16 10:21:07 +0530

    Add maximize-area-of-square-hole-in-grid.java

---
+++ maximize-area-of-square-hole-in-grid.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        int maxH = longestConsecutive(hBars) + 1;
+        int maxV = longestConsecutive(vBars) + 1;
+        int side = Math.min(maxH, maxV);
+        return side * side;
+    }
+
+    private int longestConsecutive(int[] bars) {
+        if (bars.length == 0) return 0;
+
+        Arrays.sort(bars);
+        int max = 1, curr = 1;
+
+        for (int i = 1; i < bars.length; i++) {
+            if (bars[i] == bars[i - 1] + 1) {
+                curr++;
+            } else {
+                curr = 1;
+            }
+            max = Math.max(max, curr);
+        }
+        return max;
+    }
+}
