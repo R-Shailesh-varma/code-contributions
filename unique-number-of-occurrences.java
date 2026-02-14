ff7256bdfaa883adb475dc03fbd318b94ea4043c
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2026-02-14 19:02:40 +0530

    Add unique-number-of-occurrences.java

---
+++ unique-number-of-occurrences.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i: arr){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }Set<Integer> set = new HashSet<>();
+        for(int i: map.keySet()){
+            set.add(map.get(i));
+        }
+
+        return map.keySet().size() == set.size();
+    }
+}
