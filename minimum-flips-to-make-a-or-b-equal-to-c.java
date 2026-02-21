7bbf51d276cdecafd9e502abee03e259051d18e4
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2026-02-21 23:47:24 +0530

    Add minimum-flips-to-make-a-or-b-equal-to-c.java

---
+++ minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minFlips(int a, int b, int c) {
+        int count = 0;
+        while(a > 0 || b > 0 || c > 0 ){
+            int x = (a & 1);
+            int y = (b & 1);
+            int z = (c & 1);
+            if(z == 0){
+                count += x + y;
+            }else{
+                count += (x|y) == 0 ? 1 : 0;
+            }
+            a = (a >> 1);
+            b = (b >> 1);
+            c = (c >> 1);
+        }
+
+        return count;
+    }
+}
