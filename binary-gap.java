246e4f8acce557f87ed301f1978d5a6807fa3a7a
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2026-02-22 08:24:05 +0530

    Add binary-gap.java

---
+++ binary-gap.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int binaryGap(int n) {
+        int max = 0;
+        int idx = 0;
+        int last = -1;
+        while(n > 0){
+            if((n & 1) == 1){
+                if(last != -1){
+                    max = Math.max(max, idx - last);
+                }last = idx;
+            }
+            n = (n >> 1);
+            idx++;
+        }
+        return max;
+        
+    }
+}
