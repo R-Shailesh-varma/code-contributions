9c7b9617a35913fc24f941c83abafa05b58a5db5
Racharla Shailesh Varma <racharlashailesh3116@gmail.com>
2026-04-05 14:12:43 +0530

    Add robot-return-to-origin.java

---
+++ robot-return-to-origin.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        HashMap<Character, int[]> map = new HashMap<>();
+        map.put('U', new int[]{1, 0});
+        map.put('D', new int[]{-1, 0});
+        map.put('')
+    }
+}
